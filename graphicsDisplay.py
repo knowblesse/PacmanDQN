1614971189f404f827f19c52556820ba97c50e7b
Ji Hoon Jeong <knowblesse@gmail.com>
2021-12-31 18:11:38 +0900

    unused code hunk deleted

--- graphicsDisplay.py
+++ graphicsDisplay.py
@@ -642,55 +642,6 @@ class PacmanGraphics:
                 changeColor(image, formatColor(*color))
         refresh()
 
-
-class FirstPersonPacmanGraphics(PacmanGraphics):
-
-    def __init__(self, zoom=1.0, showGhosts=True, capture=False, frameTime=0):
-        PacmanGraphics.__init__(self, zoom, frameTime=frameTime)
-        self.showGhosts = showGhosts
-        self.capture = capture
-
-    def initialize(self, state, isBlue=False):
-
-        self.isBlue = isBlue
-        PacmanGraphics.startGraphics(self, state)
-        # Initialize distribution images
-        walls = state.layout.walls
-        dist = []
-        self.layout = state.layout
-
-        # Draw the rest
-        self.distributionImages = None  # initialize lazily
-        self.drawStaticObjects(state)
-        self.drawAgentObjects(state)
-
-        # Information
-        self.previousState = state
-
-    def lookAhead(self, config, state):
-        if config.getDirection() == 'Stop':
-            return
-        else:
-            pass
-            # Draw relevant ghosts
-            allGhosts = state.getGhostStates()
-            visibleGhosts = state.getVisibleGhosts()
-            for i, ghost in enumerate(allGhosts):
-                if ghost in visibleGhosts:
-                    self.drawGhost(ghost, i)
-                else:
-                    self.currentGhostImages[i] = None
-
-    def getGhostColor(self, ghost, ghostIndex):
-        return GHOST_COLORS[ghostIndex]
-
-    def getPosition(self, ghostState):
-        if not self.showGhosts and not ghostState.isPacman and ghostState.getPosition()[1] > 1:
-            return (-1000, -1000)
-        else:
-            return PacmanGraphics.getPosition(self, ghostState)
-
-
 def add(x, y):
     return (x[0] + y[0], x[1] + y[1])
 
